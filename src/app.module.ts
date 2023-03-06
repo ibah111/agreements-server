b2f1cd8ac607880509e9bb6508ad08f41c92740b
Ibah <starlord77@mail.ru>
2023-03-06 11:55:23 +0300

    Добавил страницы

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ModuleOfModules } from './Modules';
+import { PagesModule } from './Pages/Pages.module';
 
 @Module({
-  imports: [ModuleOfModules],
+  imports: [ModuleOfModules, PagesModule],
 })
 export class AppModule {}
