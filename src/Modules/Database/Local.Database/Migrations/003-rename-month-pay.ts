767fa5229f08ed93e3b211e44ebd0d0db27d86e5
Ibah <starlord77@mail.ru>
2023-03-09 16:47:44 +0300

    создана миграция для переименования month_pay -> month_pay_day РЕДАКТИРОВАНИЕ БАЗЫ ДАННЫХ ПРОИСХОДИТ С ПОМОЩЬЮ КОДА, А НЕ В SQL

---
+++ src/Modules/Database/Local.Database/Migrations/003-rename-month-pay.ts
@@ -0,0 +1,5 @@
+import { QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.renameColumn('Agreements', 'month_pay', 'month_pay_day');
