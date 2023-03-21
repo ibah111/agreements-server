f086c49d7d770b33f9c2f9e6f17b68ab53ffe464
Ibah <starlord77@mail.ru>
2023-03-21 12:21:25 +0300

    Валидатор был перенесен в в utils как отдельный файл. Файл и функция

---
+++ src/utils/validators/IsNumberOrStringOrBoolean.ts
@@ -0,0 +1,35 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'string-or-number-or-boolean' })
+export class IsNumberOrStringOrBooleanClass
+  implements ValidatorConstraintInterface
+{
+  validate(text: unknown) {
+    return (
+      typeof text === 'number' ||
+      typeof text === 'string' ||
+      typeof text === 'boolean'
+    );
+  }
+  defaultMessage() {
+    return '($value) must be number or string or boolean';
+  }
+}
+export function IsNumberOrStringOrBoolean(
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'string-or-number-or-boolean',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: IsNumberOrStringOrBooleanClass,
+    });
+  };
+}
