1b05391c4d5bc2994ea3175266ce9bf580564550
Ibah <starlord77@mail.ru>
2023-03-22 14:22:01 +0300

    Правки

--- src/Modules/Validator/IsNumberOrString.validator.ts
+++ src/Modules/Validator/IsNumberOrString.validator.ts
@@ -1,6 +1,5 @@
 import {
   registerDecorator,
-  ValidationArguments,
   ValidationOptions,
   ValidatorConstraint,
   ValidatorConstraintInterface,
@@ -8,17 +7,17 @@ import {
 
 @ValidatorConstraint({ name: 'string-or-number', async: false })
 export class IsNumberOrStringClass implements ValidatorConstraintInterface {
-  validate(text: any, args: ValidationArguments) {
+  validate(text: unknown) {
     return typeof text === 'number' || typeof text === 'string';
   }
 
-  defaultMessage(args: ValidationArguments) {
+  defaultMessage() {
     return '($value) must be number or string';
   }
 }
 
 export function IsNumberOrString(validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
+  return function (object: object, propertyName: string) {
     registerDecorator({
       name: 'string-or-number',
       target: object.constructor,
