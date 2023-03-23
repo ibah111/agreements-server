b1c7d6c7724571c99e20cd1115bf85ca3b507d8b
Ibah <starlord77@mail.ru>
2023-03-23 12:33:10 +0300

    Добавил контроллер и модуль логина

---
+++ src/Pages/Login/Login.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { Auth, AuthGuard, AuthResult } from 'src/Modules/Guards/auth.guard';
+@UseGuards(AuthGuard)
+@Controller('login')
+export class LoginController {
+  @HttpCode(200)
+  @Post()
+  login(@Auth() auth: AuthResult) {
+    return auth.user;
+  }
+}
---
+++ src/Pages/Login/Login.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { LoginController } from './Login.controller';
+
+@Module({ controllers: [LoginController] })
+export class LoginModule {}
