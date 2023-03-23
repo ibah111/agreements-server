fa957e614bfbb5508d6dd8f1077b1ac5572f4b92
Ibah <starlord77@mail.ru>
2023-03-23 12:33:37 +0300

    Импорт в логин модуля в импорт pages

--- src/Pages/Pages.module.ts
+++ src/Pages/Pages.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AgreementsModule } from './Agreements/Agr.module';
+import { LoginModule } from './Login/Login.module';
 import PurposePage from './Purpose/Purpose.page';
 import { UserModule } from './User/User.module';
 
-@Module({ imports: [UserModule, AgreementsModule, PurposePage] })
+@Module({ imports: [LoginModule, UserModule, AgreementsModule, PurposePage] })
 export class PagesModule {}
