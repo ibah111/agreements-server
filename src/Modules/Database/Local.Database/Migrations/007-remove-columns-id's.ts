83408658c453ed5152946510b6a87f8113a75eea
Ibah <starlord77@mail.ru>
2023-04-04 16:33:05 +0300

    Миграция по удаления law act id

---
+++ src/Modules/Database/Local.Database/Migrations/007-remove-columns-id's.ts
@@ -0,0 +1,5 @@
+import { QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.removeColumn('Agreements', 'r_law_act_id');
