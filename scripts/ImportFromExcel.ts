7768fc4804a1c8966480dffaba934408bfe1ffa0
Ibah <starlord77@mail.ru>
2023-05-31 17:56:10 +0300

    [exceljs] [import-from-excel] Добавлено создание обьекта

--- scripts/ImportFromExcel.ts
+++ scripts/ImportFromExcel.ts
@@ -1,31 +1,114 @@
+import { CreationAttributes } from '@sql-tools/sequelize';
+import { Sequelize } from '@sql-tools/sequelize-typescript';
 import { program } from 'commander';
-import { Column, Row, Workbook, Worksheet } from 'exceljs';
+import { CellValue, Column, Row, Workbook, Worksheet } from 'exceljs';
+import moment from 'moment';
+import { models } from '../src/Modules/Database/Local.Database/models';
+import { Agreement } from '../src/Modules/Database/Local.Database/models/Agreement';
+import AgreementDebtsLink from '../src/Modules/Database/Local.Database/models/AgreementDebtLink';
 interface Opts {
   path: string;
 }
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite',
+  models: models,
+});
+const attributesAgremment = Object.keys(Agreement.getAttributes());
+const attributesDebt = Object.keys(AgreementDebtsLink.getAttributes());
+type ResultRow = CreationAttributes<Agreement> & {
+  DebtLinks: CreationAttributes<AgreementDebtsLink>[];
+};
+function convert(value: CellValue, name: string) {
+  return value;
+}
 async function importRunned(data: Worksheet) {
   const predata: Record<string, Row[]> = {};
-  const columns: Partial<Column>[] = [
-    { key: 'id' },
-    { key: 'law_act_id' },
-    { key: 'debt_id' },
+  const result: ResultRow[] = [];
+  const columns: (
+    | Partial<Column> & {
+        // key?: keyof Attributes<Agreement> | Attributes<AgreementDebtsLink>;
+      }
+  )[] = [
+    {},
+    {},
+    { key: 'id_debt' },
     {},
     { key: 'conclusion_date' },
     { key: 'fio' },
     { key: 'birth_date' },
     {},
     {},
+    { key: 'purpose' },
+    {},
+    { key: 'court_sum' },
+    { key: 'debt_sum' },
+    { key: 'recalculation_sum' },
+    { key: 'discount_sum' },
+    { key: 'discount' },
+    {},
+    {},
+    {},
+    { key: 'month_pay_day' },
+    {},
+    {},
+    {},
+    {},
+    { key: 'new_regDoc' },
+    { key: 'registrator' },
+    { key: 'receipt_dt' },
+    {},
+    { key: 'actions_for_get' },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    { key: 'comment' },
+    {},
+    { key: 'task_link' },
   ];
   data.columns = columns;
-  const rows = data.getRows(2, data.actualColumnCount) || [];
+  const rows = data.getRows(2, data.rowCount) || [];
   for (const row of rows) {
     const unique =
-      (row.getCell('conclusion_date').value || '').toString() +
+      (row.getCell('conclusion_date').value
+        ? moment(row.getCell('conclusion_date').value as Date).format(
+            'DD.MM.YYYY',
+          )
+        : '') +
       (row.getCell('fio').value || '').toString() +
-      (row.getCell('birth_date').value || '').toString();
+      (row.getCell('birth_date').value
+        ? moment(row.getCell('birth_date').value as Date).format('DD.MM.YYYY')
+        : '');
     if (!predata[unique]) predata[unique] = [];
     predata[unique].push(row);
   }
+  console.log(Object.keys(Agreement.getAttributes()));
+  for (const rows of Object.values(predata)) {
+    const agreement_data = { DebtLinks: [] } as unknown as ResultRow;
+    const first = rows[0];
+    for (const attribute of attributesAgremment) {
+      try {
+        const cell = first.getCell(attribute);
+        if (cell) agreement_data[attribute] = cell.value;
+      } catch {}
+    }
+    for (const row of rows) {
+      const debt_data = {} as CreationAttributes<AgreementDebtsLink>;
+      for (const attribute of attributesDebt) {
+        try {
+          const cell = first.getCell(attribute);
+          if (cell) debt_data[attribute] = convert(cell.value, attribute);
+        } catch {}
+      }
+      agreement_data.DebtLinks.push(debt_data);
+    }
+    result.push(agreement_data);
+  }
+  console.log(result);
 }
 async function main() {
   const opts = program.option('-p, --path <string>').parse().opts() as Opts;
@@ -40,8 +123,8 @@ async function main() {
   if (!worksheet_finished) return;
   const worksheet_break = workbook.worksheets[2]; // Утратившие силу
   if (!worksheet_break) return;
-  const worksheet_shit = workbook.worksheets[3]; // Соглашения об отсутпном
-  if (!worksheet_shit) return;
+  const worksheet_out = workbook.worksheets[3]; // Соглашения об отсутпном
+  if (!worksheet_out) return;
   importRunned(worksheet_runned);
 }
 main();
