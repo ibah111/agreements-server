1dac7567d37f1c69b78288cc23462623282835d5
ibah <starlord77@mail.ru>
2023-07-28 10:45:08 +0300

    feat: связка соглашения к имуществу

---
+++ src/Modules/Database/Local.Database/Migrations/033-create-agreement-to-properties.ts
@@ -0,0 +1,20 @@
+import { DataTypes, QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = async ({ context }) => {
+  await context.createTable('AgreementToProperties', {
+    id_person_property: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    id_agreement: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+  });
+};
+export const down: MigrationFn<QueryInterface> = async ({ context }) => {
+  await context.dropTable('AgreementToPropertiesd');
+};
