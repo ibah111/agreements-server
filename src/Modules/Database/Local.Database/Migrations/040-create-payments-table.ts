ffb7a0d2d1370063fb4dff38d970f3e5ef5b75a8
Ibah <starlord77@mail.ru>
2023-08-14 16:23:15 +0300

    refactor: редакт миграции, добавил cascade

--- src/Modules/Database/Local.Database/Migrations/040-create-payments-table.ts
+++ src/Modules/Database/Local.Database/Migrations/040-create-payments-table.ts
@@ -1,43 +1,49 @@
 import { DataTypes, QueryInterface } from '@sql-tools/sequelize';
 import { MigrationFn } from 'umzug';
 
-export const up: MigrationFn<QueryInterface> = async ({ context }) => {
-  const transaction = await context.sequelize.transaction();
-  context.createTable('Payments', {
-    id_agreement: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    pay_day: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    sum_owe: {
-      type: DataTypes.MONEY,
-      allowNull: false,
-    },
-    sum_payed: {
-      type: DataTypes.MONEY,
-    },
-    status: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  });
-  try {
-    transaction.commit;
-  } catch (error) {
-    console.log(error);
-    transaction.rollback();
-  }
-};
-export const down: MigrationFn<QueryInterface> = async ({ context }) => {
-  const transaction = await context.sequelize.transaction();
-  context.dropTable('Payments');
-  try {
-    transaction.commit();
-  } catch (error) {
-    transaction.rollback();
-  }
-};
+export const up: MigrationFn<QueryInterface> = async ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.createTable('Payments', {
+        id_agreement: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        pay_day: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        sum_owe: {
+          type: DataTypes.MONEY,
+          allowNull: false,
+        },
+        sum_payed: {
+          type: DataTypes.MONEY,
+        },
+        status: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+        },
+      }),
+      context.addConstraint('Payments', {
+        name: 'payments_id_agreement_fk',
+        fields: ['id_agreement'],
+        type: 'foreign key',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          field: 'id_agreement',
+          table: 'Agreements',
+        },
+        transaction: t,
+      }),
+    ]),
+  );
+export const down: MigrationFn<QueryInterface> = async ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.dropTable('Payments', { transaction: t }),
+      context.removeConstraint('Payments', 'payments_id_agreement_fk'),
+    ]),
+  );
