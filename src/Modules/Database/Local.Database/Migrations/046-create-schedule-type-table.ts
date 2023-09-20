625ed678ea67ac717960da697251d9a6a69071b2
ibah <starlord77@mail.ru>
2023-09-20 09:27:34 +0300

    fix: поправил миграции

--- src/Modules/Database/Local.Database/Migrations/046-create-schedule-type-table.ts
+++ src/Modules/Database/Local.Database/Migrations/046-create-schedule-type-table.ts
@@ -2,27 +2,28 @@ import { QueryInterface, DataTypes } from '@sql-tools/sequelize';
 import { MigrationFn } from 'umzug';
 
 export const up: MigrationFn<QueryInterface> = async ({ context }) =>
-  await context.sequelize.transaction(async (t) =>
-    Promise.all([
-      await context.createTable(
-        'ScheduleType',
-        {
-          id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true,
+  await context.sequelize.transaction(
+    async (t) =>
+      await Promise.all([
+        context.createTable(
+          'ScheduleType',
+          {
+            id: {
+              type: DataTypes.INTEGER,
+              autoIncrement: true,
+              allowNull: false,
+              primaryKey: true,
+            },
+            title: {
+              type: DataTypes.STRING,
+              allowNull: false,
+            },
+            createdAt: { type: DataTypes.DATE, allowNull: false },
+            updatedAt: { type: DataTypes.DATE, allowNull: false },
           },
-          title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          createdAt: { type: DataTypes.DATE, allowNull: false },
-          updatedAt: { type: DataTypes.DATE, allowNull: false },
-        },
-        { transaction: t },
-      ),
-    ]),
+          { transaction: t },
+        ),
+      ]),
   );
 export const down: MigrationFn<QueryInterface> = async ({ context }) =>
   await context.sequelize.transaction(async (t) =>
