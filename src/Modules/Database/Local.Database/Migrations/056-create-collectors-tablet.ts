3780aac4d9ab66e35555620602beede20066ddb6
Ibah <starlord77@mail.ru>
2023-11-07 13:48:23 +0300

    feat(collector-table): миграция, создал таблицу

---
+++ src/Modules/Database/Local.Database/Migrations/056-create-collectors-tablet.ts
@@ -0,0 +1,35 @@
+import { DataTypes, QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = async ({ context }) => {
+  await context.sequelize.transaction(async (t) => {
+    Promise.all([
+      await context.createTable(
+        'Collectors',
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          id_contact: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+          fio: {
+            type: DataTypes.STRING,
+          },
+          department_name: {
+            type: DataTypes.STRING,
+          },
+        },
+        { transaction: t },
+      ),
+    ]);
+  });
+};
+export const down: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([context.dropTable('Collectors', { transaction: t })]),
+  );
